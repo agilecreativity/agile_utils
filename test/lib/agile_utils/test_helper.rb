54d65848b7e1787265b352331c014d98c69df9b4
Burin Choomnuan <agilecreativity@gmail.com>
2014-04-28 20:48:01 +1000

    WIP

--- test/lib/agile_utils/test_helper.rb
+++ test/lib/agile_utils/test_helper.rb
@@ -5,7 +5,6 @@ describe AgileUtils::Helper do
       result = AgileUtils::Helper.shell(%w(ls))
       result.wont_be_nil
     end
-
     it 'raises error on invalid command' do
       assert_raises(RuntimeError) { AgileUtils::Helper.shell(%w(bad-command)) }
     end
