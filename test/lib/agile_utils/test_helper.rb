af42e60b3e705c5f1f7c4a9ffc8eb5582aa94c82
Burin Choomnuan <agilecreativity@gmail.com>
2014-04-28 20:44:41 +1000

    WIP

--- test/lib/agile_utils/test_helper.rb
+++ test/lib/agile_utils/test_helper.rb
@@ -2,14 +2,26 @@ require_relative '../../test_helper'
 describe AgileUtils::Helper do
   context '#shell' do
     it 'returns result for valid command' do
-      #TODO: very system specific, please mock/stub this out!
-      # or use something like FakeFS?
       result = AgileUtils::Helper.shell(%w(ls))
       result.wont_be_nil
     end
 
     it 'raises error on invalid command' do
       assert_raises(RuntimeError) { AgileUtils::Helper.shell(%w(bad-command)) }
+    end
+  end
+
+  context '#which' do
+    it 'works with valid executable' do
+      # TODO: mock or stub the call
+      AgileUtils::Helper.which('ruby').wont_be_nil
+    end
+    it 'works with invalid executable' do
+      AgileUtils::Helper.which('bad-or-invalid-executable!').must_be_nil
+    end
+    it 'works with the binary name only' do
+      # TODO: mock or stub the call
+      AgileUtils::Helper.which('/bin/bash').must_be_nil
     end
   end
 end
